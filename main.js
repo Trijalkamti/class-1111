c0275dc8a6bcd92d15210759e81732df8bcc462d
Trijalkamti <82204283+Trijalkamti@users.noreply.github.com>
2021-11-15 12:50:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+Webcam.set({
+    widht:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera")
+
+Webcam.attach( '#camera' );
+
+function snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' +data_uri+'"/>';
+
+    });
+}
+
+console.log('ml5 version:' , ml5.version);
+classifier = ml5.imageClassifier('',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first predication is " + predication_1;
+    speak_data_2 = "The second predication is " + predication_2;
+    var utterThis = SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        predication_1 = results[0].label;
+        predication_2 = results[1].label;
+        speak();
+        if(results[0].label == "happy")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128522;";
+        }
+        if(results[0].label == "sad")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128532";
+        }
+        if(results[0].label == "angry")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128548";
+        }
+
+    }
+}
